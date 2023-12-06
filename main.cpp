cba701ece7de480bed2617ab7935754fdc96b76e
Ikumi1930 <115620514+Ikumi1930@users.noreply.github.com>
2023-12-06 17:55:17 +0900

    a

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <stdio.h>
+#include <thread>
+
+std::random_device seed_Gen;
+std::mt19937 mtrand(seed_Gen());
+
+typedef int (*Pfunc)();
+
+int RollingDice() { return std::uniform_int_distribution<int>(1, 6)(seed_Gen); }
+
+void DiceResult(int diceNumber) {
+	// 偶数
+	if (diceNumber % 2 == 0) {
+		printf("結果　丁\n");
+	}
+	// 奇数
+	if (diceNumber % 2 == 1) {
+		printf("結果　半\n");
+	}
+}
+
+int WaitingTime(Pfunc returnValue, int second) {
+	std::this_thread::sleep_for(std::chrono::seconds(second));
+	return returnValue();
+}
+
+int main() {
+
+	Pfunc p;
+	p = RollingDice;
+	int Result;
+	int Answer;
+
+	while (true) {
+
+		printf("1:半　2:丁\n");
+		std::cin >> Answer;
+
+		if (Answer == 0) {
+			break;
+		}
+
+		Result = WaitingTime(p, 3);
+		printf("答え : %d\n", Result);
+		DiceResult(Result);
+
+		if (Result % 2 == 0 && Answer == 2) {
+			printf("正解\n");
+		}
+
+		else if (Result % 2 == 1 && Answer == 1) {
+			printf("正解\n");
+		}
+
+		else {
+			printf("不正解\n");
+		}
+	}
+
+	return 0;
+}
